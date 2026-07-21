f2a0c28d531587acfae7f0f0a620d59374352797
Christian <christian@christiankirkeby.com>
2026-07-21 13:15:18 +1000

    feat: migrate to an Astro template

--- components/Main/Main.tsx
+++
@@ -1,9 +0,0 @@
-import styles from "./Main.module.css";
-
-interface MainProps {
-  children: React.ReactNode;
-}
-
-export const Main = ({ children }: MainProps) => {
-  return <main className={styles.main}>{children}</main>;
-};
--- components/index.tsx
+++
@@ -1,9 +0,0 @@
-export { Card } from "components/Card/Card";
-export { Page } from "components/Page/Page";
-export { Meta } from "components/Meta/Meta";
-export { Header } from "components/Header/Header";
-export { Main } from "components/Main/Main";
-export { Footer } from "components/Footer/Footer";
-export { MDX } from "components/MDX/MDX";
-export { Logo } from "components/Logo/Logo";
-export { Code, type CodeProps } from "components/Code/Code";
